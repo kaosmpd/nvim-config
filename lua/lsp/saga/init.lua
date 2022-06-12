3c888682b0ac54f5fc717a865b68435e3ceeea16
Nguyen Nhat Hao <nguyennhathao@yandex.com>
2022-06-12 16:57:28 +0700

    feat(lspsaga): Disable code action sign icon

--- lua/lsp/saga/init.lua
+++ lua/lsp/saga/init.lua
@@ -1,2 +1,10 @@
 local saga = require("lspsaga")
-saga.init_lsp_saga({})
+saga.init_lsp_saga({
+  code_action_icon = "",
+  code_action_prompt = {
+    enable = false,
+    sign = false,
+    sign_priority = 40,
+    virtual_text = true,
+  },
+})
